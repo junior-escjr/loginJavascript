4dee07d4ece64fc3c1a980988fa5e1511d27a52d
https://github.com/juniorbdb/loginJavascript.git <edsonjunior@MacBook-Pro-de-Junior.local>
2014-06-07 01:16:53 -0300

    decimo

--- www/js/usuario.js
+++ www/js/usuario.js
@@ -2,25 +2,25 @@ data = {
 
 	init: function(){
 		//var btn = document.getElementById("login");
-		document.addEventListener("deviceready", data.valorUsuarioSenha, false);
+		document.addEventListener("deviceready", this.valorUsuarioSenha, false);
 		//this.verificaUsuario();
 	},
 
 	valorUsuarioSenha: function(){
 
-		// var dados = {
-		//     usuario: $("#login-adm").val(),
-		//     pass: $("#senha_adm").val()
-		//   };
-		// this.verificaUsuario(dados);
-		alert("asdasdasd");
+		var dados = {
+		    usuario: $("#login-adm").val(),
+		    pass: $("#senha_adm").val()
+		  };
+
+		data.verificaUsuario(dados);
 		
 	},
 
 	verificaUsuario: function(dados){
 		var user = dados.usuario;
 		var senha = dados.pass;
-		alert(t);
+		alert(user);
 	}
 
 	
